90857d586abd182bc6a96d751385bf3aa21e853e
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-12 09:47:44 +0900

    Initial Commit

---
+++ Shared/ProjectTrackingIntroComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTrackingIntroComponentApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/05/12.
+//
+
+import SwiftUI
+
+@main
+struct ProjectTrackingIntroComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
